9a7ae8df534104fc11574f5069d40aa7384bc1b9
morsgiathatch <corbinwilliambaldwin@gmail.com>
2019-12-27 02:27:12 -0700

    Make additions a function for code clarity

--- src/output-json-dns.h
+++ src/output-json-dns.h
@@ -30,5 +30,6 @@ void JsonDnsLogRegister(void);
 
 json_t *JsonDNSLogQuery(void *txptr, uint64_t tx_id) __attribute__((nonnull));
 json_t *JsonDNSLogAnswer(void *txptr, uint64_t tx_id) __attribute__((nonnull));
+void PrintIPAddressFromJsonDNSLogAnswer(json_t *answer);
 
 #endif /* __OUTPUT_JSON_DNS_H__ */
